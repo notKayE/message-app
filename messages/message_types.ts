9430671f5a02ef25625b077ddf5daef629429e51
JoakimBorg <info@joakimborg.se>
2025-02-20 09:36:31 +0100

    First draft of messages done.

--- messages/message_types.ts
+++ messages/message_types.ts
@@ -1,4 +1,13 @@
+import { Queue } from "../lib/queue_array"
+
 export type Message = {
   sender: string,
-  message: string,
+  body: string,
+}
+
+export type TempUser = {
+  name: string
+  password: string
+  message_queue: Queue<Message | unknown>
+  friends: Array<TempUser>
 }
--- messages/messages_functions.ts
+++ messages/messages_functions.ts
@@ -1,45 +1,70 @@
 import { dequeue, empty, enqueue, head, Queue } from '../lib/queue_array';
-import { Message } from './message_types';
+import { Message, TempUser } from './message_types';
 
 // Three renamed queue functions.
 export const message_create_queue = () => empty();
 export const message_enqueue = (message: Message, queue: Queue<Message | unknown>): void => enqueue(message, queue);
 export const message_dequeue = (queue: Queue<Message | unknown>): void => dequeue(queue);
 
-// Creates a message with a sender.
-function message_create(sender: string, message: string,): Message {
-  return { sender, message };
+// Creates a message from a User.
+function message_create(user: TempUser, body: string,): Message {
+  return { sender: user.name, body };
 }
 
-// Sends a message to a recipient.
+// Sends a message to a recipient. Preconditions that the recipient exists
+// TODO: Find recipient and check if exists on top level.
+// TODO: Update type User to match TempUser.
+function message_send(recipient: TempUser, message: Message): void {
+  message_enqueue(message, recipient.message_queue)
+  console.log(`Message sent to ${recipient.name}.\n`)
+}
 
 // Reads a message then dequeues it.
-function message_read_and_dequeue(queue: Queue<Message | unknown>): void {
-  // If queue is empty.
-  if (!head(queue)) return console.log('All messages read.');
+function message_read_and_dequeue(user: TempUser): void {
+  // If queue is empty, return.
+  if (!head(user.message_queue)) return console.log('All messages read.');
 
-  const { sender, message } = head(queue) as Message;
+  const { sender, body } = head(user.message_queue) as Message;
 
   console.log(`${sender} says:`)
-  console.log(message)
-  console.log();
+  console.log(`${body}\n`)
 
-  message_dequeue(queue);
+  message_dequeue(user.message_queue);
 }
 
+// -----------------------------
 // Tests
 const q = message_create_queue();
-const p = message_create_queue();
 
-const m1 = message_create('Ivan', 'Hej, hur mår du?');
-const m2 = message_create('Elis', 'Toppen, ska vi ses på fredag?');
-const m3 = message_create('Joakim', 'Klockan 10 blir bra.');
+const joakim: TempUser = {
+  name: 'Joakim',
+  password: '123',
+  message_queue: message_create_queue(),
+  friends: [],
+}
 
-message_enqueue(m1, q);
-message_enqueue(m2, q);
-message_enqueue(m3, q);
+const ivan: TempUser = {
+  name: 'Ivan',
+  password: '123',
+  message_queue: message_create_queue(),
+  friends: [],
+}
 
-message_read_and_dequeue(q);
-message_read_and_dequeue(q);
-message_read_and_dequeue(q);
-message_read_and_dequeue(q);
+const elis: TempUser = {
+  name: 'Elis',
+  password: '123',
+  message_queue: message_create_queue(),
+  friends: [],
+}
+
+const m1 = message_create(ivan, 'Hej, hur mår du?');
+const m2 = message_create(elis, 'Toppen, ska vi ses på fredag?');
+const m3 = message_create(ivan, 'Klockan 10 blir bra.');
+
+message_send(joakim, m1)
+message_send(joakim, m2)
+message_send(joakim, m3)
+
+message_read_and_dequeue(joakim)
+message_read_and_dequeue(joakim)
+message_read_and_dequeue(joakim)
